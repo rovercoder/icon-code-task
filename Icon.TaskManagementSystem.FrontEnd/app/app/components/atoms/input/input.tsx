cf4f64441b0cac1142180a7432415cdb64daacc0
Nathan Zahra <nathanzahra@hotmail.com>
2026-01-23 18:22:06 +0100

    Cleanup

--- Icon.TaskManagementSystem.FrontEnd/app/app/components/atoms/input/input.tsx
+++ Icon.TaskManagementSystem.FrontEnd/app/app/components/atoms/input/input.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './input.css';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
